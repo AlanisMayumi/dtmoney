95a765c44413c668594ae4d29bee60da21b296d2
AlanisMayumi <mayumi.alanis@gmail.com>
2021-07-28 23:39:05 -0300

    aula_finalizada_hook

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactonModal: () => void;
+}
+export function Header({ onOpenNewTransactonModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactonModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -14,18 +14,20 @@ export const Content = styled.div`
   justify-content: space-between;
 
   button {
-      font-size: 1rem;
-      color: #FFF;
-      background: var(--blue-light);
-      border: 0;
-      padding: 0 2rem;
-      border-radius: 0.25rem;
-      height: 3rem;
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
 
-      transition: filter 0.2s;
+    transition: filter 0.2s;
 
-      &:hover {
-          filter: brightness(0.9);
-      }
+    &:hover {
+      filter: brightness(0.9);
+    }
   }
 `;
+
+
